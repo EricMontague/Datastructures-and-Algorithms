31bb144bd35525b8f93c7bb6f079f9095a3fb501
Eric Montague <eric.g.montague@gmail.com>
2020-02-22 18:38:01 -0500

    initial commit, union-find

---
+++ data_structures/union_find.py
@@ -0,0 +1,80 @@
+"""This module contains an implementation of the Union 
+Find data structure. It utilizes union by rank and
+path compression, which allow the time complexities
+of union and find to be amortized constant time.
+"""
+
+class UnionFind:
+    """Class to represent the Union Find data structure."""
+
+    def __init__(self, size):
+        if size <= 0:
+            raise ValueError("Size must be greater than 0.")
+        self.parent = [None] * size
+        self.rank = [None] * size
+        self.num_components = size
+        self.size = size
+
+    def make_set(self, element):
+        """Create a new disjoint set with that contains
+        only the given element.
+        """
+        #initially, element points to itself and rank is 0
+        self.parent[element] = element
+        self.rank[element] = 0
+    
+    def find(self, element):
+        """Return the name of the set that the given element
+        belongs to.
+        """
+        #traverse up the tree until we hit the root
+        root = element
+        while root != self.parent[root]:
+            root = self.parent[root]
+
+        #path compression
+        #traverse up the tree pointing each element
+        #to the root, until we hit the root itself
+
+        while element != self.parent[element]:
+            parent = self.parent[element]
+            self.parent[element] = root
+            element = parent
+        return root
+
+    #recursive find operation with path compression
+    # def find(self, element):
+    #     """Return the name of the set that the given
+    #     element belongs to.
+    #     """
+    #     if element != self.parent[element]:
+    #         self.parent[element] = self.find(self.parent[element])
+    #     return self.parent[element]
+
+    def union(self, element_one, element_two):
+        """Merge the two sets that both of the given
+        elements belong to."""
+        root_one = self.find(element_one)
+        root_two = self.find(element_two)
+
+        #if both roots are the same, both elements are
+        #in the same set, so we don't merge anything
+
+        if root_one != root_two:
+            #merge set with lower rank into set with higher rank
+            if self.rank[root_one] > self.rank[root_two]:
+                self.parent[root_two] = root_one
+            elif self.rank[root_one] < self.rank[root_two]:
+                self.parent[root_one] = root_two
+            else: #Tied rank. Merge arbitrarily and increase rank
+                self.parent[root_one] = root_two
+                self.rank[root_two] += 1
+            self.num_components -= 1 #decrease num components
+
+    def get_num_components(self):
+        """Return the number components in the UnionFind."""
+        return self.num_components
+
+    def is_connected(self, element_one, element_two):
+        """Return True if both elements are in the same set."""
+        return self.find(element_one) == self.find(element_two)
---
+++ tests/test_union_find.py
@@ -0,0 +1,95 @@
+"""This module holds tests for my implementation
+of the Union Find data structure.
+"""
+import unittest
+from data_structures.union_find import UnionFind
+
+
+class UnionFindTestCase(unittest.TestCase):
+    """Class to run tests on the Union Find implementation."""
+
+    def test_invalid_size(self):
+        """Test to ensure that an exception is raised
+        when the UnionFind is instantiated with a size
+        that isn't greater than 0.
+        """
+        with self.assertRaises(ValueError):
+            union_find = UnionFind(0)
+        
+        with self.assertRaises(ValueError):
+            union_find = UnionFind(-1)
+
+        with self.assertRaises(ValueError):
+            union_find = UnionFind(-2349)
+
+    def test_initial_rank_zero(self):
+        """Test that a set's initial rank is 0
+        when created.
+        """
+        initial_rank = 0
+        elements = [0, 1, 2, 3]
+        union_find = UnionFind(4)
+        for element in elements:
+            union_find.make_set(element)
+            self.assertEqual(union_find.rank[element], initial_rank)
+
+    def test_root_points_to_itself(self):
+        """Test to ensure that the root of the set
+        points to itself when created.
+        """
+        elements = [0, 1, 2, 3]
+        union_find = UnionFind(4)
+        for element in elements:
+            union_find.make_set(element)
+            self.assertEqual(union_find.find(element), element)
+
+    def test_is_connected(self):
+        """Test to ensure UnionFind reports that elements
+        are in the same set after a union operation
+        is performed.
+        """
+        elements = [0, 1, 2, 3]
+        union_find = UnionFind(4)
+        for element in elements:
+            union_find.make_set(element)
+            #element should be connected to itself
+            self.assertTrue(union_find.is_connected(element, element))
+        
+        union_find.union(0, 1)
+        self.assertTrue(union_find.is_connected(0, 1))
+        self.assertTrue(union_find.is_connected(1, 0))
+        self.assertFalse(union_find.is_connected(0, 2))
+        self.assertFalse(union_find.is_connected(0, 3))
+        self.assertFalse(union_find.is_connected(1, 2))
+        self.assertFalse(union_find.is_connected(1, 3))
+        self.assertFalse(union_find.is_connected(3, 2))
+
+    def test_num_components(self):
+        """Test to ensure UnionFind reports the
+        corrent number of components after a union
+        operation.
+        """
+        elements = [0, 1, 2, 3]
+        union_find = UnionFind(4)
+        for element in elements:
+            union_find.make_set(element)
+        self.assertEqual(union_find.get_num_components(), 4)
+
+        union_find.union(0, 1)
+        self.assertEqual(union_find.get_num_components(), 3)
+
+        union_find.union(2, 3)
+        self.assertEqual(union_find.get_num_components(), 2)
+
+        union_find.union(1, 3)
+        self.assertEqual(union_find.get_num_components(), 1)
+
+        #since both are now in the same set, the number of
+        #components should stay the same
+        union_find.union(1, 2)
+        self.assertEqual(union_find.get_num_components(), 1)
+        
+
+if __name__ == "__main__":
+    unittest.main() 
+        
