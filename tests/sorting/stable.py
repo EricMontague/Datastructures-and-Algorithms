830b28459075a2416b848b0d78a91580e2b8c290
Eric Montague <eric.g.montague@gmail.com>
2020-04-11 11:52:24 -0400

    created helper module to be used to test if sorting algorithms are stable or not

---
+++ tests/sorting/stable.py
@@ -0,0 +1,15 @@
+"""This module contains a class that is used with helping to test
+whether a sorting algorithm is stable or not.
+"""
+
+
+class TestProduct:
+    """Dummy class created to test the stability property of sorting algorithms."""
+
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+
+    def __lt__(self, other):
+        return self.quantity < other.quantity
+        
