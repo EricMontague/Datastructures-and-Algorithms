82c4db3fe5542e634b5ac50cbd8e4e4e8df715c9
Eric Montague <eric.g.montague@gmail.com>
2020-04-12 11:54:56 -0400

    renamed vairables to be more descriptive

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,14 +1,14 @@
 """This module contains my implementation of bubble sort."""
 
 
-def bubble_sort(input_list):
+def bubble_sort(unsorted_list):
     """Implementation of bubble sort. Sorts an unsorted list
     in-place."""
-    for i in range(len(input_list) - 1):
+    for i in range(len(unsorted_list) - 1):
         swapped = False
-        for j in range(len(input_list) - i - 1):
-            if input_list[j] > input_list[j + 1]:
-                input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
+        for j in range(len(unsorted_list) - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
                 swapped = True
         if not swapped:
             break
