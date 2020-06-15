f38cfbb130ecd8dd7b8bce4419c0ba33aa91b79c
Eric Montague <eric.g.montague@gmail.com>
2020-06-15 10:22:08 -0400

    added encapsulation to instance variables

--- data_structures/stacks/stack_using_array.py
+++ data_structures/stacks/stack_using_array.py
@@ -5,29 +5,33 @@ class Stack:
     """Class to represent a stack."""
 
     def __init__(self):
-        self.items = []
-        self.size = 0
-    
+        self._items = []
+        self._size = 0
+
     def push(self, item):
         """Insert at the top of the stack."""
-        self.items.append(item)
-        self.size += 1
-    
+        self._items.append(item)
+        self._size += 1
+
     def pop(self):
         """Remove and return the top item in the stack."""
         if self.is_empty():
             raise IndexError("Stack is empty.")
-        item = self.items.pop()
-        self.size -= 1
+        item = self._items.pop()
+        self._size -= 1
         return item
-    
+
     def is_empty(self):
         """Return True if the stack is empty, else return False."""
-        return self.size == 0
-    
+        return self._size == 0
+
     def peek(self):
         """Return the value of the item on top of the stack."""
         if self.is_empty():
             return None
-        return self.items[-1]
-    
+        return self._items[-1]
+
+    @property
+    def size(self):
+        """Return the number of items on the stack."""
+        return self._size
