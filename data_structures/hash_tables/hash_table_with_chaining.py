9a3920138edab8c73bdca4bb938a49c04efb82a3
Eric Montague <eric.g.montague@gmail.com>
2020-07-09 12:37:14 -0400

    added rehash method to hash table as well as the necessary methods to decide whether to double or halve the table size. Also I fixed the set_item() method to allow for updating of a key-value pair if the key passed in already exists in the hash table.

--- data_structures/hash_tables/hash_table_with_chaining.py
+++ data_structures/hash_tables/hash_table_with_chaining.py
@@ -12,14 +12,15 @@ class HashItem:
         self.key = key
         self.value = value
         self.next = None
-        
+
 
 class HashTable:
     """Class to represent a hash table."""
 
     def __init__(self):
         self._buckets = [None] * 8
-    
+        self._num_items = 0
+
     def get_item(self, key):
         """Return the item associated with the given key."""
         index = self._hash(key, len(self._buckets))
@@ -35,12 +36,20 @@ class HashTable:
         already a value associated with the given key, then the
         value will be overwritten.
         """
-        index = self._hash(key, len(self._buckets))
-        head = self._buckets[index]
-        new_item = HashItem(key, value)
-        if head is not None:
-            new_item.next = head
-        self._buckets[index] = new_item
+        # check whether key exists in hash table already
+        item = self.get_item(key)
+        if item is not None:
+            item.value = value
+        else:
+            index = self._hash(key, len(self._buckets))
+            head = self._buckets[index]
+            new_item = HashItem(key, value)
+            if head is not None:
+                new_item.next = head
+            self._buckets[index] = new_item
+            self._num_items += 1
+            if self._should_double():
+                self._rehash(2)
 
     def delete_item(self, key):
         """Delete an item in the hash table with the given key."""
@@ -51,7 +60,7 @@ class HashTable:
         # Remove from head
         if head.key == key:
             self._buckets[index] = self._buckets[index].next
-        else: # Remove from middle or end
+        else:  # Remove from middle or end
             target = head.next
             while True:
                 if target is None:
@@ -61,7 +70,43 @@ class HashTable:
                     break
                 target = target.next
                 head = head.next
-            
+        self._num_items -= 1
+        if self._should_halve():
+            self._rehash(0.5)
+
+    def exists(self, key):
+        """Return True if there is a value associated with the given key 
+        in the hash table.
+        """
+        return self.get_item(key) is not None
+
+    def _hash(self, key, num_buckets):
+        """Map the given key to a value from 0 to num_buckets - 1."""
+        return hash(key) % num_buckets
+
+    def _should_double(self):
+        """Return True if the table size should be doubled."""
+        # double table size if half of the slots are occupied
+        return self._num_items == len(self._buckets) // 2:
+
+    def _should_halve(self):
+        """Return True if the table size should be halved."""
+        # halve table size if only a quarter of the slots are occupied
+        return self._num_items == len(self._buckets) // 4:
+         
+    def _rehash(self, factor):
+        """Rehash the contents of the current hash table into a new
+        table by growing or shrinking the table, depending on the factor
+        that is passed in.
+        """
+        items = self._buckets.copy()
+        self._buckets = [None] * len(self._buckets) * factor
+        self._num_items = 0
+        for item in items:
+            while item:
+                self.set_item(item.key, item.value)
+                item = item.next
+
     def __getitem__(self, key):
         """"Return the item associated with the given key."""
         return self.get_item(key)
@@ -72,26 +117,14 @@ class HashTable:
         value will be overwritten.
         """
         self.set_item(key, value)
-    
+
     def __delitem__(self, key):
         """Delete an item in the hash table with the given key."""
         self.delete_item(key)
-    
+
     def __contains__(self, key):
         """Return True if there is a value associated with the given key
         in the hash table.
         """
         return self.get_item(key) is not None
-        
-    def _hash(self, key, num_buckets):
-        """Map the given key to a value from 0 to num_buckets - 1."""
-        return hash(key) % num_buckets
-    
-    def _grow(self):
-        """Double the size of the hash table."""
-        pass
-
-    def _shrink(self):
-        """Halve the size of the hash table."""
-        pass
 
