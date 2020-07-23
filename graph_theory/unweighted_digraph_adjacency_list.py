6cc1df6b0007e37a86f3c50bb933dfb701e8b48c
Eric Montague <eric.g.montague@gmail.com>
2020-07-23 15:23:25 -0400

    finished implementations of unweighted digraphs minus the traversal algorithms

---
+++ graph_theory/unweighted_digraph_adjacency_list.py
@@ -0,0 +1,101 @@
+"""This module contains my implementations of an unweighted directed graph
+represented as an adjacency list. I assume that all of the vertices (inputs) 
+are positive integers.
+"""
+
+
+
+class UnweightedDiGraph:
+    """Class to represent an unweighted directed graph using
+    an adjacency list.
+    """
+
+    def __init__(self):
+        self._num_vertices = 0
+        self._adjacency_list = {}
+
+    def add_vertex(self, vertex):
+        """Add a new vertex to the graph."""
+        if vertex < 0:
+            raise ValueError("A vertex can't be less than 0")
+        if not self.has_vertex(vertex):
+            self._adjacency_list[vertex] = set()
+            self._num_vertices += 1
+
+    def remove_vertex(self, vertex):
+        """Remove a vertex from the graph, along with its
+        outgoing and incoming edges.
+        """
+        if self._num_vertices == 0:
+            raise ValueError("Graph has no vertices")
+        if not self.has_vertex(vertex):
+            raise ValueError("The given vertex doesn't exist within the graph")
+        self._adjacency_list.pop(vertex)
+        self._num_vertices -= 1
+        for neighbors in self._adjacency_list.values():
+            if vertex in neighbors:
+                neighbors.remove(vertex)
+    
+    def add_edge(self, source, destination):
+        """Add an edge to the graph."""
+        # Add both vertices to graph first if they don't exist
+        if not self.has_vertex(source) and not self.has_vertex(destination):
+            self.add_vertex(source)
+            self.add_vertex(destination)    
+        # Add destination vertex to graph first if it doesn't exist
+        elif self.has_vertex(source) and not self.has_vertex(destination):
+            self.add_vertex(destination)
+        # Add source vertex to graph first if it doesn't exists
+        elif not self.has_vertex(source) and self.has_vertex(destination):
+            self.add_vertex(source)
+        self._adjacency_list[source].add(destination)
+
+    def remove_edge(self, source, destination):
+        """Remove an edge from the graph."""
+        if self._num_vertices == 0:
+            raise ValueError("Graph has no vertices")
+        if not self.has_vertex(source):
+            raise ValueError(f"Vertex {source} doesn't exist in the graph")
+        neighbors = self._adjacency_list[source]
+        if destination in neighbors:
+            neighbors.remove(destination)
+
+    def has_vertex(self, vertex):
+        """Return True if the given vertex exists in the graph."""
+        return vertex in self._adjacency_list
+
+    def has_edge(self, source, destination):
+        """Return True if an edge exists from the source vertex
+        to the destination vertex.
+        """
+        if self._adjacency_list.get(source) is not None:
+            return destination in self._adjacency_list[source]
+        return False
+
+    @property
+    def num_vertices(self):
+        """Return the number of vertices in the graph."""
+        return self._num_vertices
+
+    @property
+    def num_edges(self):
+        """Return the number of edges in the graph."""
+        edges = 0
+        for neighbors in self._adjacency_list.values():
+            edges += len(neighbors)
+        return edges
+
+    def traverse(self, algorithm):
+        pass
+
+    def _dfs(self):
+        pass
+
+    def _dfs_visit(self, source, visited):
+        pass
+
+    def _bfs(self, source):
+        pass
+
+
+
---
+++ graph_theory/unweighted_digraph_adjacency_matrix.py
@@ -0,0 +1,110 @@
+"""This module contains my implementations of an unweighted directed graph
+represented as an adjacency matrix. I assume that all of the vertices (inputs) 
+are positive integers.
+"""
+
+
+class UnWeightedDiGraph2:
+    """Class to represent an unweighted, directed graph
+    using an adjacency matrix."""
+
+    def __init__(self, num_vertices):
+        if num_vertices < 1:
+            raise ValueError("num_vertices cannot be less than 1")
+        self._num_vertices = num_vertices
+        self._adjacency_matrix = [[0] * num_vertices for vertex in range(num_vertices)]
+    
+    def add_vertex(self):
+        """Add a new vertex to the graph."""
+        self._num_vertices += 1
+        for row in self._adjacency_matrix:
+            row.append(0)
+        self._adjacency_matrix.append([0] * self._num_vertices)
+
+    def remove_vertex(self, vertex):
+        """Remove the given vertex from the graph."""
+        if vertex < 0 or vertex >= self._num_vertices:
+            raise ValueError(f"Vertices must be between 0 and {self._num_vertices - 1}")
+        if not self.has_vertex(vertex):
+            raise ValueError("Vertex does not exist in the graph")
+        # Shift all rows after the vertex (index) that you are trying to remove back one
+        for row in range(vertex + 1, self._num_vertices):
+            self._adjacency_matrix[row - 1] = self._adjacency_matrix[row]
+        self._adjacency_matrix[-1].pop() # remove last row as it is now a duplicate
+
+        # Shift all columns after the vertex (index) that you are trying to remove back one
+        for row in range(self._num_vertices):
+            for col in range(vertex + 1, self._num_vertices):
+                self._adjacency_matrix[row][col - 1] = self._adjacency_matrix[row][col]
+            self._adjacency_matrix[row].pop()
+        self._num_vertices -= 1
+
+    def add_edge(self, source, destination):
+        """Add an edge to the graph."""
+        if self.has_vertex(source) and self.has_vertex(destination):
+            self._adjacency_matrix[source][destination] = 1
+        elif not self.has_vertex(source) and not self.has_vertex(destination):
+            self.add_vertex() # add source
+            self.add_vertex() # add destintation
+            self._adjacency_matrix[-2][-1] = 1
+        elif self.has_vertex(source) and not self.has_vertex(destination):
+            self.add_vertex()
+            self._adjacency_matrix[source][-1] = 1
+        elif not self.has_vertex(source) and self.has_vertex(destination):
+            self.add_vertex()
+            self._adjacency_matrix[-1][destination] = 1
+
+    def remove_edge(self, source, destination):
+        """Remove an edge from the graph."""
+        if self._num_vertices == 0:
+            raise ValueError("Graph is empty")
+        if not self.has_vertex(source):
+            raise ValueError(f"Vertex {source} not in graph")
+        if not self.has_vertex(destination):
+            raise ValueError(f"Vertex {destination} not in graph")
+        self._adjacency_matrix[source][destination] = 0
+
+    @property
+    def num_vertices(self):
+        """Return the number of vertices in the graph."""
+        return self._num_vertices
+
+    @property
+    def num_edges(self):
+        """Return the number of vertices in the graph."""
+        edges = 0
+        for row in range(self._num_vertices):
+            for col in range(self._num_vertices):
+                if self._adjacency_matrix[row][col] == 1:
+                    edges += 1
+        return edges
+
+    def has_vertex(self, vertex):
+        """Return True if the graph contains the given vertex."""
+        try:
+            return self._adjacency_matrix[vertex] is not None
+        except IndexError:
+            return False
+
+    def has_edge(self, source, destination):
+        """Return True if the graph contains an edge from the source
+        vertex to the destination vertex.
+        """
+        try:
+            return self._adjacency_matrix[source][destination] == 1
+        except IndexError:
+            return False
+
+    def traverse(self, algorithm):
+        pass
+
+    def _dfs(self):
+        pass
+
+    def _dfs_visit(self, source, visited):
+        pass
+
+    def _bfs(self, source):
+        pass
+
+
