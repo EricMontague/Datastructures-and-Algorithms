df5a301c2ae4b9f6e71cf561257ebdd60b129b70
Eric Montague <eric.g.montague@gmail.com>
2020-08-18 23:02:35 -0400

    Added version of quicksort that using Hoare's  partioning scheme

--- sorting/quicksort2.py
+++ sorting/quicksort2.py
@@ -3,20 +3,42 @@ using Hoare's partitioning algorithm.
 """
 
 
+def swap(list_, index_one, index_two):
+    """Swap the elements at the two indices in the list."""
+    list_[index_one], list_[index_two] = list_[index_two], list_[index_one]
+
+
 def hoare_partition(unsorted_list, start, end):
     """Given an unsorted list and indices that mark the start
     and end of a segment in the list, partition the list
     around a pivot and return the index of that pivot.
     """
-    pass
+    pivot_index = (start + end) // 2
+    pivot_value = unsorted_list[pivot_index]
+    left = start
+    right = end
+    while True:
+        while unsorted_list[left] < pivot_value:
+            left += 1
+        while unsorted_list[right] > pivot_value:
+            right -= 1
+        if left >= right:
+            return right
+        swap(unsorted_list, left, right)
+        left += 1
+        right -= 1
 
 
 def quicksort(unsorted_list, start, end):
     """Implementation of Quicksort algorithm. Given an unsorted
     list of elements, sort that list in increasing order.
     """
-    if len(unsorted_list) > 1:
-        pivot_index = hoare_partition(unsorted_list, start, end)
-        quicksort(unsorted_list, start, pivot_index - 1)
-        quicksort(unsorted_list, pivot_index + 1, end)
+    if start < end:
+        split_index = hoare_partition(unsorted_list, start, end)
+        quicksort(unsorted_list, start, split_index)
+        quicksort(unsorted_list, split_index + 1, end)
 
+
+array = [2, 3, 1, 5, 3, 6]
+quicksort(array, 0, len(array) - 1)
+print(array)
