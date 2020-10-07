d1f4dbdbbbea30e982ec8e5dc27d032e16e99a18
Eric Montague <eric.g.montague@gmail.com>
2020-10-07 16:50:44 -0400

    Refactored AVL Tree. First , I added null checks in the insert and delete methods. Next, I moved the update_balance_factor and rebalance methods into the insert and delete methods themselves. This is more efficient in the case of the update method as it now gets called on roughly log n nodes instead of n nodes. I also added a small optimization to the delete method;s two children case. lastly, the code in the rebalance method was cleaned up and the should_rotate_twice method was removed

--- data_structures/balanced_trees/avl_tree.py
+++ data_structures/balanced_trees/avl_tree.py
@@ -7,6 +7,7 @@ class AVLTreeNode:
         self.left = None
         self.right = None
         self.balance_factor = 0
+        self.height = 0
 
 
 class AVLTree:
@@ -14,9 +15,9 @@ class AVLTree:
         self.root = None
 
     def insert(self, data):
+        if data is None:
+            raise ValueError("Node value cannot be None")
         self.root = self._insert(self.root, data)
-        self._update_balance_factors(self.root)
-        self.root = self._rebalance_tree(self.root)
 
     def _insert(self, current_node, data):
         if not current_node:
@@ -25,12 +26,13 @@ class AVLTree:
             current_node.left = self._insert(current_node.left, data)
         else:
             current_node.right = self._insert(current_node.right, data)
-        return current_node
+        self._update_balance_factor_and_height(current_node)
+        return self._rebalance(current_node)
 
     def delete(self, data):
+        if data is None:
+            raise ValueError("Node value cannot be None")
         self.root = self._delete(self.root, data)
-        self._update_balance_factors(self.root)
-        self._rebalance_tree(self.root)
 
     def _delete(self, current_node, data):
         if not current_node:
@@ -47,66 +49,81 @@ class AVLTree:
             elif not current_node.right:
                 current_node = current_node.left
             else:
-                max_node = self._find_max(current_node.left)
-                current_node.val = max_node.val
-                current_node.left = self._delete(current_node.left, max_node.val)
-        return current_node
+                # small optimization to prevent unecessary rebalancing
+                if current_node.left.height > current_node.right.height:
+                    max_node = self._find_max(current_node.left)
+                    current_node.data = max_node.data
+                    current_node.left = self._delete(
+                        current_node.left, current_node.data
+                    )
+                else:
+                    min_node = self._find_min(current_node.right)
+                    current_node.data = min_node.data
+                    current_node.right = self._delete(
+                        current_node.right, current_node.data
+                    )
 
-    def _find_max(self, root):
-        if not root:
-            return None
-        if not root.right:
-            return root
-        return self._find_max(root.right)
+        self._update_balance_factor_and_height(current_node)
+        return self._rebalance(current_node)
 
-    def _update_balance_factors(self, current_node):
+    def _find_max(self, current_node):
         if not current_node:
-            return -1
-        left_subtree_height = self._update_balance_factors(current_node.left)
-        right_subtree_height = self._update_balance_factors(current_node.right)
+            return None
+        if not current_node.right:
+            return current_node
+        return self._find_max(current_node.right)
+
+    def _find_min(self, current_node):
+        if not current_node:
+            return None
+        if not current_node.left:
+            return current_node
+        return self._find_min(current_node.left)
+
+    def _update_balance_factor_and_height(self, current_node):
+        if not current_node:
+            return
+        left_subtree_height = -1
+        right_subtree_height = -1
+        if current_node.left:
+            left_subtree_height = current_node.left.height
+        if current_node.right:
+            right_subtree_height = current_node.right.height
+        current_node.height = max(left_subtree_height, right_subtree_height) + 1
         current_node.balance_factor = right_subtree_height - left_subtree_height
-        return max(left_subtree_height, right_subtree_height) + 1
 
-    def _rebalance_tree(self, root):
-        new_root = root
+    def _rebalance(self, node):
+        if not node:
+            return None
+        new_node = node
         # right heavy
-        if root.balance_factor == 2:
-            if self._should_rotate_twice(root):
-                root.right = self._rotate_right(root.right)
-            new_root = self._rotate_left(root)
+        if node.balance_factor == 2:
+            if node.right.balance_factor < 0:  # zig-zag, need two rotations
+                node.right = self._rotate_right(node.right)
+            new_node = self._rotate_left(node)
         # left heavy
-        elif root.balance_factor == -2:
-            if self._should_rotate_twice(root):
-                root.left = self._rotate_left(root.left)
-            new_root = self._rotate_right(root)
-        return new_root
+        elif node.balance_factor == -2:
+            if node.left.balance_factor > 0:  # zig-zag, need two rotations
+                node.left = self._rotate_left(node.left)
+            new_node = self._rotate_right(node)
+        return new_node
 
-    def _should_rotate_twice(self, root):
-        # root has a left child. but not a right child
-        if root.left and not root.right:
-            # left child has no left child, but has a right child
-            if not root.left.left and root.left.right:
-                return True
-
-        # root has a right child, but not a left child
-        if root.right and not root.left:
-            # right child ha not right child, but has a left child
-            if not root.right.right and root.right.left:
-                return True
-        return False
-
-    def _rotate_left(self, root):
-        right_child = root.right
+    def _rotate_left(self, node):
+        right_child = node.right
         right_child_left_subtree = right_child.left
-        right_child.left = root
-        root.right = right_child_left_subtree
+        right_child.left = node
+        node.right = right_child_left_subtree
+        self._update_balance_factor_and_height(right_child)
+        self._update_balance_factor_and_height(node)
         return right_child
 
-    def _rotate_right(self, root):
-        left_child = root.left
+    def _rotate_right(self, node):
+        left_child = node.left
         left_child_right_subtree = left_child.right
-        left_child.right = root
-        root.left = left_child_right_subtree
+        left_child.right = node
+        node.left = left_child_right_subtree
+        self._update_balance_factor_and_height(left_child)
+        self._update_balance_factor_and_height(node)
         return left_child
 
     def __str__(self):
@@ -126,6 +143,8 @@ class AVLTree:
 
 # Small informal test. Will make an official test later
 balanced = True
+
+
 def is_balanced(root):
     global balanced
     if not root:
@@ -139,6 +158,7 @@ def is_balanced(root):
 # test insertions
 avl_tree = AVLTree()
 for value in range(5):
+    print(value)
     avl_tree.insert(value)
 is_balanced(avl_tree.root)
 print(balanced)
