170bc4bb345b63ebf044645cfbf576539e7c6f95
Eric Montague <eric.g.montague@gmail.com>
2020-10-12 19:22:04 -0400

    Refactored the bellman ford algorithm into multiple functions

--- graph_theory/algorithms/bellman_ford_adjacency_matrix.py
+++ graph_theory/algorithms/bellman_ford_adjacency_matrix.py
@@ -11,7 +11,7 @@ def bellman_ford_adjacency_matrix(source, graph):
     predecessors = {source: None}
 
     # Relax v - 1 edges
-    for _ in range(len(graph) - 1):
+    for i in range(len(graph) - 1):
         for node in range(len(graph)):
             for neighbor, weight in enumerate(graph[node]):
                 if weight != 0:
@@ -21,26 +21,61 @@ def bellman_ford_adjacency_matrix(source, graph):
                         predecessors[neighbor] = node
 
     # Final relaxation attempt to identify negative cycles
+    detect_negative_cycles(graph, distances, predecessors)
+    return distances, predecessors
+
+
+def detect_negative_cycles(graph, distances, predecessors):
     for node in range(len(graph)):
         for neighbor, weight in enumerate(graph[node]):
             if weight != 0:
                 new_distance = distances[node] + weight
                 if new_distance < distances[neighbor]:  # negative weight cycle found
                     distances[neighbor] = float("-inf")
-                    predecessors.pop(neighbor)
-    return distances, predecessors
+                    predecessors[neighbor] = -1
 
 
 def find_shortest_path(predecessors, source, destination):
     """Given a dictionary of predeccesors that was constructed via
-    a shortest paths algorithm, return the vertices in the shortest
+    the Bellman-Ford algorithm, return the vertices in the shortest
     path from source to destination.
     """
     path = []
+    # destination vertex not reachable from source
+    if destination not in predecessors:
+        return path
+
     current_node = destination
     while current_node is not None:
         predecessor = predecessors[current_node]
+        if predecessor == -1:  # vertex is part of a negative weight cycle
+            return None
         path.append(current_node)
         current_node = predecessor
     path.reverse()
     return path
+
+
+# Graph from:
+# https://www.geeksforgeeks.org/bellman-ford-algorithm-simple-implementation/
+def test_bellman_ford():
+    graph = [
+        [0, -1, 4, float("inf"), float("inf")],
+        [float("inf"), 0, 3, 2, 2],
+        [float("inf"), float("inf"), 0, float("inf"), float("inf")],
+        [float("inf"), 1, 5, 0, float("inf")],
+        [float("inf"), float("inf"), float("inf"), -3, 0]
+    ]
+    start = 0
+    end = 3
+    distances, predecessors = bellman_ford_adjacency_matrix(start, graph)
+    shortest_path = find_shortest_path(predecessors, start, end)
+
+    print("Vertex       Distance from Source")
+    for node in distances:
+        print(f"{node}                    {distances[node]}")
+    print()
+    print(f"Shortest path from {start} to {end}: ", shortest_path)
+
+
+test_bellman_ford()
