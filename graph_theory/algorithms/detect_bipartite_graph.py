95918e36c4fbc4a8d0d293865121d2af2a8a62b0
Eric Montague <eric.g.montague@gmail.com>
2020-10-19 15:41:01 -0400

    Added a module that will eventually house algorithms for detecting a bipartite graph.

---
+++ graph_theory/algorithms/detect_bipartite_graph.py
@@ -0,0 +1,11 @@
+"""This module contains an algorithm to detect whether a graph is Bipartite or not.
+
+A Bipartite graph is a graph whose vertices can be grouped into two sets, U and V, such
+that each edge connects a vertex in set U to a vertex to set V.
+
+Another way of looking at this is that the graph is 2-colorable, meaning that given two colors, 
+you can assign colors to vertices such that no two adjacent vertices share the same color.
+
+Yet another way of reasoning about this is that no vertex in either set, U or V, can share an
+edge with another vertex in the same set.
+"""
