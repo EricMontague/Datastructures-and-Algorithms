c7418a2f45d4490cd7b1419cbec8e4a72fb4552f
Eric Montague <eric.g.montague@gmail.com>
2020-10-19 15:42:01 -0400

    updated variable names to improve readability

--- graph_theory/algorithms/strongly_connected_components/tarjans_algorithm.py
+++ graph_theory/algorithms/strongly_connected_components/tarjans_algorithm.py
@@ -32,64 +32,66 @@ pop nodes off of the stack that are in that component. You know that a node star
 connected component if it's id equals its low link value
 """
 
+_UNVISITED = -1
 
 # the graph is assumed to be an adjacency list
 def tarjans_algorithm(source, graph):
 
     # Used to assign ids and intial low link values to nodes
-    counter = 0
+    node_id = 0
 
     # Initialize node ids and low link values lists
     # A value of -1 means that node has not yet been visited
-    node_ids = [-1] * len(graph)
-    low_link_values = [-1] * len(graph)
+    node_ids = [_UNVISITED] * len(graph)
+    low_link_values = [_UNVISITED] * len(graph)
 
     # Initialize stack to hold nodes during dfs and a set to
     # check whether a given node is in the stack
     stack = []
-    in_stack = set()
+    in_stack = [False] * len(graph)
 
     # Iterate over all nodes since the graph may not be fully connected
     for node in range(len(graph)):
         # Only perform DFS from a node that has not yet been visited
-        if node_ids[node] == -1 and low_link_values[node] == -1:
-            updated_counter = find_strongly_connected_components(
-                node, graph, node_ids, low_link_values, stack, in_stack, counter
+        if node_ids[node] == _UNVISITED:
+            updated_node_id = find_strongly_connected_components(
+                node, graph, node_ids, low_link_values, stack, in_stack, node_id
             )
-            counter = updated_counter
+            node_id = updated_node_id
     return low_link_values
 
 
 def find_strongly_connected_components(
-    current_node, graph, node_ids, low_link_values, stack, in_stack, counter
+    current_node, graph, node_ids, low_link_values, stack, in_stack, node_id
 ):
     # Assign the node's id and low link value to be the current value of the counter
-    node_ids[current_node] = counter
+    node_ids[current_node] = node_id
     low_link_values[current_node] = node_ids[current_node]
 
-    # Increment counter
-    updated_counter = counter + 1
+    # Increment node_id
+    updated_node_id = node_id + 1
 
-    # Push node onto the stack and add it to the in_stack set
+    # Push node onto the stack and mark it as being in the stack
     stack.append(current_node)
-    in_stack.add(current_node)
+    in_stack[current_node] = True
 
     # Iterate over adjacenct neighbors
     for neighbor in graph[current_node]:
         # Check if adjacent node has been visited
-        if node_ids[neighbor] == -1 and low_link_values[neighbor] == -1:
-            updated_counter = find_strongly_connected_components(
+        if node_ids[neighbor] == _UNVISITED:
+            updated_node_id = find_strongly_connected_components(
                 neighbor,
                 graph,
                 node_ids,
                 low_link_values,
                 stack,
                 in_stack,
-                updated_counter,
+                updated_node_id,
             )
         # If the neighbor is NOT in the stack, then it must be a part of a different SCC
         # If it is in the stack, then update this current node's low link value
-        if neighbor in in_stack:
+        # This is what allows the low link values to propagate throughout the cycles in the graph
+        if in_stack[neighbor]:
             low_link_values[current_node] = min(
                 low_link_values[current_node], low_link_values[neighbor]
             )
@@ -98,14 +100,19 @@ def find_strongly_connected_components(
     # The low link value of a node that is the start of a SCC won't have changed
     # during the DFS traversal and will be equal to its node_id
     if node_ids[current_node] == low_link_values[current_node]:
+        # If you wanted to count the number of SCC, here is where you would
+        # increment your counter for that purpose
 
         # Remove all nodes that are a part of this SCC from the stack
+        # And marked them as removed
         while stack and low_link_values[stack[-1]] == node_ids[current_node]:
             top_node = stack.pop()
-            in_stack.remove(top_node)
-    return updated_counter
+            in_stack[top_node] = False
+    return updated_node_id
 
 
+# Test case taken from here https://www.youtube.com/watch?v=wUgWX0nc4NY
+# with some additional vertices added
 def test_tarjans_algorithm():
     graph = {
         0: [1],
@@ -116,6 +123,9 @@ def test_tarjans_algorithm():
         5: [0, 6],
         6: [2, 0, 4],
         7: [5, 3],
+        8: [9],
+        9: [10],
+        10: [8],
     }
     source = 4
     strongly_connected_components = tarjans_algorithm(source, graph)
