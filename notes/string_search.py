ab7c12202f93b3fbbacc3e9c89bd2eb7fff31558
Eric Montague <eric.g.montague@gmail.com>
2020-10-23 18:47:16 -0400

    Added notes for string search algorithms

---
+++ notes/string_search.py
@@ -0,0 +1,23 @@
+"""This module contains notes related to algorithms designed to
+search for a pattern within a large text string.
+
+
+
+- a string is a sequence of characters over some alphabet.
+Some example alphabets are binary, decimal, ASCII, UNICODE, and DNA
+- Some applications for pattern matching algorithms include:
+    - Parsers
+    - Lexis/Nexis
+    - Spam filters
+    - Virus scanning
+    - Digital libraries
+    - Screen scrapers
+    - Word processors
+    - Web search engines
+    - Sybmol manipulation
+    - Bibliographic retrieval
+    - Natrual language processing
+    - Carnivore surveiallance system
+    - Computational molecular biology
+    - Feature detection in digitized images
+"""
