16da118ab0448b7c68c90b1b7cbe6dda10cdc19e
Eric Montague <eric.g.montague@gmail.com>
2020-11-23 14:58:06 -0500

    Redid the unweighted digraph adjacency list implementation and added an object oriented representation as well.

--- graph_theory/directed_graphs/unweighted_digraph_object_oriented.py
+++ graph_theory/directed_graphs/unweighted_digraph_object_oriented.py
@@ -1,21 +1,47 @@
-"""This module contains my implementations of an unweighted directed graph
-represented as an adjacency list.
+"""This module contains an implementation of an unweighted directed graph
+built in an object oriented way.
 """
 
-from data_structures.linked_lists.singly_linked_list_no_tail_pointer import (
-    SinglyLinkedList,
-)
+
+class UnweightedDiGraphNode:
+    """Class to represent a node in an unweighted directed graph."""
+
+    def __init__(self, node_id):
+        self._node_id = node_id
+        self._neighbors = {}
+
+    @property
+    def node_id(self):
+        """Return the id for this node."""
+        return self._node_id
+
+    @property
+    def neighbors(self):
+        """Return a list of this node's neighbors."""
+        return list(self._neighbors.values())
+
+    def has_neighbor(self, node_id):
+        """Return True of the given node is a neighbor of this
+        node, otherwise return False.
+        """
+        return node_id in self._neighbors
+
+    def add_neighbor(self, node):
+        """Add the given node as a neighbor for this node."""
+        self._neighbors[node.node_id] = node
+
+    def remove_neighbor(self, node_id):
+        """Remove the given node from the dictionary of neighbors."""
+        if not self.has_neighbor(node_id):
+            raise ValueError(f"{node_id} is not a neighbor of {self._node_id}")
+        self._neighbors.pop(node_id)
 
 
 class UnweightedDiGraph:
-    """Class to represent an unweighted directed graph using
-    an adjacency list.
-    """
-
     def __init__(self):
         self._num_nodes = 0
         self._node_id = 0
-        self._adjacency_list = {}
+        self._nodes = {}
 
     # O(1)
     def add_node(self):
@@ -23,25 +49,25 @@ class UnweightedDiGraph:
         for the new node.
         """
         id = self._node_id
-        self._adjacency_list[id] = SinglyLinkedList()
+        self._nodes[id] = UnweightedDiGraphNode(id)
         self._node_id += 1
         self._num_nodes += 1
         return id
 
-    # O(V + E)
+    # O(1)
     def remove_node(self, node_id):
         """Remove the node from the graph with the given id."""
         if not self.has_node(node_id):
             raise ValueError(f"Graph does not have node: {node_id}")
-        self._adjacency_list.pop(node_id)
-        for node in self._adjacency_list:
+        node_to_remove = self._nodes.pop(node_id)
+        for node in self._nodes:
             try:
-                self._adjacency_list[node].remove(node_id)
+                self._nodes[node].remove_neighbor(node_id)
             except ValueError:
                 pass
         self._num_nodes -= 1
 
-    # O(deg+(V)), # outdegree of V
+    # O(1)
     def add_edge(self, from_, to):
         """Create an edge between the two given nodes. If either don't exist in
         the graph already, create both nodes before forming the edge.
@@ -50,21 +76,19 @@ class UnweightedDiGraph:
             from_ = self.add_node()
         if not self.has_node(to):
             to = self.add_node()
-        if self.has_edge(from_, to):
-            raise ValueError(f"Edge from {from_} to {to} already exists")
-        self._adjacency_list[from_].push_front(to)
+        self._nodes[from_].add_neighbor(to)
         return from_, to
 
-    # O(deg+(V)), # outdegree of V
+    # O(1)
     def remove_edge(self, from_, to):
         """Remove the edge between the two given nodes."""
         if not self.has_node(from_):
             raise ValueError(f"Graph does not have node: {from_}")
         if not self.has_node(to):
             raise ValueError(f"Graph does not have node: {to}")
-        self._adjacency_list[from_].remove(to)
+        self._nodes[from_].remove_neighbor(to)
 
-    # O(deg+(V)), # outdegree of V
+    # O(1)
     def has_edge(self, from_, to):
         """Return True if an edge exists between the two given nodes,
         otherwise return False
@@ -73,19 +97,19 @@ class UnweightedDiGraph:
             raise ValueError(f"Graph does not have node: {from_}")
         if not self.has_node(to):
             raise ValueError(f"Graph does not have node: {to}")
-        return self._adjacency_list[to].has_node(to)
+        self._nodes[from_].has_neighbor(to)
 
     # O(1)
     def has_node(self, node_id):
         """Return True if the given node exists in the graph."""
-        return node_id in self._adjacency_list
-    
+        return node_id in self._nodes
+
     # O(deg+(V)), # outdegree of V
     def get_neighbors(self, node_id):
         """Return a list of neighbors for the given node."""
         if not self.has_node(node_id):
             return []
-        return self._adjacency_list[node_id].deep_copy()
+        return self._nodes[node_id].neighbors
 
     @property
     def num_nodes(self):
